f64ec888cb436996564247f2ca7446756eb9ae56
rasool <r.fakoor@gmail.com>
2025-12-08 14:48:23 -0800

    this mid checkpoint. under development tho

--- main_sl.py
+++ main_sl.py
@@ -1,3 +1,4 @@
+import os
 import random
 import numpy as np
 import argparse
@@ -5,10 +6,13 @@ import deepspeed
 import torch
 from transformers import AutoModel, AutoTokenizer, AutoModelForCausalLM, AutoConfig
 from torch.utils.data import DataLoader, DistributedSampler
+import torch.distributed as torch_dist
+from tqdm import tqdm
 
-# local imports
+# imports local methods, classes, etc.
 import config.load as cfg # all config arguments
-import datasets    as ds  # our custom pytorch dataset
+from custom_datasets.paired_dataset import PairedDataset # our custom pytorch dataset
+from misc.utils import save_checkpoint
 
 def set_random_seeds(seed):
     '''
@@ -17,6 +21,25 @@ def set_random_seeds(seed):
     random.seed(seed)
     np.random.seed(seed)
     torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+
+def rank_world_size_setup(config):
+    rank = int(os.environ.get('RANK', 0))
+    world_size = int(os.environ.get('WORLD_SIZE', 1))
+    return rank, world_size
+
+def batch_size_setup(train_batch_size, val_batch_size, rank, world_size):
+    '''
+        Set batch size for each gpu/rank.
+        This function helps to avoid having to do this in multiple places especially
+        with distributed training engine sometime causes confusion.
+    '''
+    bsz = {}
+    bsz['train_global'] = train_batch_size
+    bsz['train_local']  = train_batch_size // world_size
+    bsz['val_global']   = val_batch_size
+    bsz['val_local']    = val_batch_size // world_size
+    return bsz
 
 def load_models_and_tokenizer(model_name,
                               model_dtype,
@@ -90,14 +113,13 @@ def training_engine_setup(deepspeed_config, model, ref_model=None):
 
     return model_engine, ref_model_engine, optimizer
 
-def inference_engine_setup(deepspeed_config, model):
-    '''
-        This function is responsible for setting up distributed inference engine.
-        For now, it only supports deepspeed.
-    ''' 
-    return None
-
-def data_loader_setup(data_config, batch_size, tokenizer, split='train', world_size=1, rank=0):
+def data_loader_setup(data_config,
+                      batch_size,
+                      tokenizer,
+                      seed,
+                      split='train',
+                      world_size=1,
+                      rank=0):
     '''
        This function is responsible for setting up data loader.
        batch_size is an input to handle global or micro batch size.
@@ -105,11 +127,11 @@ def data_loader_setup(data_config, batch_size, tokenizer, split='train', world_s
     ########
     # 1. Initialize our custom datasets
     ########
-    dataset = ds.PairedDataset(prompt_key=data_config.prompt_key,
-                                    answer_key=data_config.answer_key,
-                                    max_seq_len=data_config.max_seq_len,
-                                    tokenizer=tokenizer,
-                                    data_path=data_config.data_path)
+    dataset = PairedDataset(prompt_key=data_config.prompt_key,
+                               answer_key=data_config.answer_key,
+                               max_seq_len=data_config.max_seq_len,
+                               tokenizer=tokenizer,
+                               data_path=data_config.data_path)
     shuffle = True if split == 'train' else False
 
     ########
@@ -133,7 +155,14 @@ def data_loader_setup(data_config, batch_size, tokenizer, split='train', world_s
                             drop_last=True,
                             )
 
-    return dataloader
+    return dataloader, sampler
+
+def inference_engine_setup(deepspeed_config, model):
+    '''
+        This function is responsible for setting up distributed inference engine.
+        For now, it only supports deepspeed.
+    '''
+    return None
 
 if __name__ == "__main__":
     # parse arguments
@@ -153,6 +182,11 @@ if __name__ == "__main__":
     # 2. Generic setup (e.g., random seed, device, world size, etc.)
     ########
     set_random_seeds(config)
+    rank, world_size = rank_world_size_setup(config)
+    batch_sizes = batch_size_setup(train_batch_size=config.train.batch_size,
+                                   val_batch_size=config.val.batch_size,
+                                   rank=rank,
+                                   world_size=world_size)
 
     ########
     # 3. Logging and saving (e.g., W&B, results dir, etc.)
@@ -173,27 +207,77 @@ if __name__ == "__main__":
     ########
     # 6. Build env or data loader
     ########
-    dataloader = data_loader_setup(config, tokenizer)    
+    train_dataloader, train_sampler = data_loader_setup(data_config=config.data,
+                                                        batch_size=config.train.batch_size,
+                                                        tokenizer=tokenizer,
+                                                        seed=config.train.seed,
+                                                        split='train',
+                                                        world_size=world_size,
+                                                        rank=rank)
+    val_dataloader, _ = data_loader_setup(data_config=config.data,
+                                          batch_size=config.train.batch_size,
+                                          tokenizer=tokenizer,
+                                          seed=config.train.seed,
+                                          split='val',
+                                          world_size=world_size,
+                                          rank=rank)
 
     ########
     # 7. Intitate the learning algorithm (e.g., ppo)
     ########
-    if str.lower(config.train.alg_name) in {'ppo', 'grpo', 'dummy'}:
-        if str.lower(config.train.alg_name) == 'dummy':
-            import algs.DUMMY.dummy as al
-            alg = al.Dummy()
+    if str.lower(config.train.alg_name) in {'sft'}:
+        if str.lower(config.train.alg_name) == 'sft':
+            import algs.SFT.sft as calg
+            alg = calg.SFT(
+                model_engine=model_engine,
+                optimizer=optimizer,
+                micro_batch_size_per_gpu=config.train.micro_batch_size_per_gpu,
+                clip_grad_norm=None,
+                use_cache=False,
+                device='cpu')
 
     else:
         raise ValueError(f"Unknown algorithm: {config.train.alg_name}")
-    
+
     ########
     # 8. Training and evaluation loop
     ########
-    alg.train(config, model_engine, ref_model_engine, optimizer, dataloader, inference_engine)
+    for epoch in range(config.train.num_epochs):
+        train_sampler.set_epoch(epoch)
+        ########
+        # 8.1 Training loop
+        ########
+        total_train_step = 0
+        for data in tqdm(train_dataloader,
+                        total=config.train.steps_per_epoch,
+                        desc=f"Epoch {epoch + 1}/{config.train.num_epochs}"):
+            total_train_step += 1
+            metric = alg.train_step(data)
+            if total_train_step % config.train.log_interval == 0:
+                print(f"Global step: {total_train_step}, Loss: {metric['loss']}")
+
+        ########
+        # 8.2 Validation loop
+        ########
+        total_eval_step = 0
+        for data in tqdm(val_dataloader,
+                        total=config.val.steps_per_epoch,
+                        desc=f"Epoch {epoch + 1}/{config.train.num_epochs}"):
+            total_eval_step += 1
+            metric = alg.eval_step(data)
+            if total_eval_step % config.eval.log_interval == 0:
+                print(f"Global step: {global_step}, Loss: {metric['loss']}")
+
+        ########
+        # 8.3 Save checkpoint
+        ########
+        if epoch % config.train.save_interval == 0:
+            save_checkpoint(config, model, optimizer, epoch)
 
     ########
     # 9. Save final checkpoint
     ########
+    save_checkpoint(config, model, optimizer, config.train.num_epochs)
 
     
 
