e028e3cae2cd4c5c2782bde65f42b77638257c00
rasool <r.fakoor@gmail.com>
2025-12-08 16:55:07 -0800

    make necessary changes to the main so it can work with the recent changes. it still requires udpdating deepseep configs

--- main_sl.py
+++ main_sl.py
@@ -23,22 +23,30 @@ def set_random_seeds(seed):
     torch.manual_seed(seed)
     torch.cuda.manual_seed_all(seed)
 
-def rank_world_size_setup(config):
+def rank_world_size_setup(world_size):
+    '''
+        Set rank and world size for distributed training.
+    '''
     rank = int(os.environ.get('RANK', 0))
     world_size = int(os.environ.get('WORLD_SIZE', 1))
     return rank, world_size
 
-def batch_size_setup(train_batch_size, val_batch_size, rank, world_size):
+def batch_size_setup(train_batch_size_per_gpu, gradient_accumulation, val_batch_size_per_gpu, world_size):
     '''
         Set batch size for each gpu/rank.
         This function helps to avoid having to do this in multiple places especially
         with distributed training engine sometime causes confusion.
+        (global) train_batch_size = micro_batch_size x gradient_accumulation x number_of_gpus
     '''
     bsz = {}
-    bsz['train_global'] = train_batch_size
-    bsz['train_local']  = train_batch_size // world_size
-    bsz['val_global']   = val_batch_size
-    bsz['val_local']    = val_batch_size // world_size
+    global_train_batch_size = train_batch_size_per_gpu * gradient_accumulation * world_size
+    bsz['train_global'] = global_train_batch_size
+    bsz['train_local']  = train_batch_size_per_gpu
+    global_val_batch_size = val_batch_size_per_gpu * world_size
+    bsz['val_global']   = global_val_batch_size
+    bsz['val_local']    = val_batch_size_per_gpu
+
+    # update related arguments in deepspeed config
     return bsz
 
 def load_models_and_tokenizer(model_name,
@@ -113,7 +121,13 @@ def training_engine_setup(deepspeed_config, model, ref_model=None):
 
     return model_engine, ref_model_engine, optimizer
 
-def data_loader_setup(data_config,
+def data_loader_setup(dnames,
+                      dataset_ratios,
+                      files_path,
+                      num_workers,
+                      max_seq_len,
+                      prompt_key,
+                      answer_key,
                       batch_size,
                       tokenizer,
                       seed,
@@ -127,11 +141,11 @@ def data_loader_setup(data_config,
     ########
     # 1. Initialize our custom datasets
     ########
-    dataset = PairedDataset(prompt_key=data_config.prompt_key,
-                               answer_key=data_config.answer_key,
-                               max_seq_len=data_config.max_seq_len,
-                               tokenizer=tokenizer,
-                               data_path=data_config.data_path)
+    dataset = PairedDataset(prompt_key=prompt_key,
+                            answer_key=answer_key,
+                            max_seq_len=max_seq_len,
+                            tokenizer=tokenizer,
+                            data_path=files_path)
     shuffle = True if split == 'train' else False
 
     ########
@@ -168,7 +182,7 @@ if __name__ == "__main__":
     # parse arguments
     parser = argparse.ArgumentParser()
     parser.add_argument("--config-file", type=str, default="./config/dummy.yaml", help="config file")
-    parser.add_argument("--run_id", type=str, default="run_1", help="run id")
+    parser.add_argument("--experiment_id", type=str, default="run_1", help="experiment id")
     parser.add_argument("--world_size", type=int, default=1, help="world size")
 
     args = parser.parse_args()
@@ -176,16 +190,18 @@ if __name__ == "__main__":
     ########
     # 1. Load config
     ########
-    config = cfg.load_and_verify(args.config_file)
+    config = cfg.load_and_verify(input_yaml=args.config_file,
+                                 experiment_id=args.experiment_id,
+                                 world_size=args.world_size)
 
     ########
     # 2. Generic setup (e.g., random seed, device, world size, etc.)
     ########
-    set_random_seeds(config)
-    rank, world_size = rank_world_size_setup(config)
-    batch_sizes = batch_size_setup(train_batch_size=config.train.batch_size,
-                                   val_batch_size=config.val.batch_size,
-                                   rank=rank,
+    set_random_seeds(seed=config.run.seed)
+    rank, world_size = rank_world_size_setup(world_size=config.run.world_size)
+    batch_sizes = batch_size_setup(train_batch_size_per_gpu=config.train.train_batch_size_per_gpu,
+                                   gradient_accumulation=config.train.gradient_accumulation_steps,
+                                   val_batch_size_per_gpu=config.train.val_batch_size_per_gpu,
                                    world_size=world_size)
 
     ########
@@ -196,28 +212,47 @@ if __name__ == "__main__":
     ########
     # 4. load model or previous checkpoints
     ########
-    model, ref_model, tokenizer = load_models_and_tokenizer(config)
+    model, ref_model, tokenizer = load_models_and_tokenizer(model_name=config.model.name,
+                                                            model_dtype=config.model.dtype,
+                                                            ref_model_name=config.model.ref_model,
+                                                            trust_remote_code=config.model.trust_remote_code,
+                                                            model_class=config.model.model_class)
 
     ########
     # 5. Setup trainiing and inference engines
     ########
-    model_engine, ref_model_engine, optimizer = training_engine_setup(config, model, ref_model)
+    model_engine, ref_model_engine, optimizer = training_engine_setup(deepspeed_config=config.deepspeed,
+                                                                      model=model,
+                                                                      ref_model=ref_model)
     inference_engine = inference_engine_setup(config, model)
 
     ########
     # 6. Build env or data loader
     ########
-    train_dataloader, train_sampler = data_loader_setup(data_config=config.data,
-                                                        batch_size=config.train.batch_size,
+    train_dataloader, train_sampler = data_loader_setup(dnames=config.data.train_dnames,
+                                                        dataset_ratios=config.data.train_ratios,
+                                                        files_path=config.data.train_files_path,
+                                                        num_workers=config.data.num_workers,
+                                                        max_seq_len=config.data.max_seq_len,
+                                                        prompt_key=config.data.prompt_key,
+                                                        answer_key=config.data.answer_key,
+                                                        batch_size=batch_sizes['train_local'],
                                                         tokenizer=tokenizer,
-                                                        seed=config.train.seed,
+                                                        seed=config.run.seed,
                                                         split='train',
                                                         world_size=world_size,
                                                         rank=rank)
-    val_dataloader, _ = data_loader_setup(data_config=config.data,
-                                          batch_size=config.train.batch_size,
+
+    val_dataloader, _ = data_loader_setup(dnames=config.data.train_dnames,
+                                          dataset_ratios=config.data.train_ratios,
+                                          files_path=config.data.val_files_path,
+                                          num_workers=config.data.num_workers,
+                                          max_seq_len=config.data.max_seq_len,
+                                          prompt_key=config.data.prompt_key,
+                                          answer_key=config.data.answer_key,
+                                          batch_size=batch_sizes['val_local'],
                                           tokenizer=tokenizer,
-                                          seed=config.train.seed,
+                                          seed=config.run.seed,
                                           split='val',
                                           world_size=world_size,
                                           rank=rank)
@@ -229,12 +264,12 @@ if __name__ == "__main__":
         if str.lower(config.train.alg_name) == 'sft':
             import algs.SFT.sft as calg
             alg = calg.SFT(
-                model_engine=model_engine,
-                optimizer=optimizer,
-                micro_batch_size_per_gpu=config.train.micro_batch_size_per_gpu,
-                clip_grad_norm=None,
-                use_cache=False,
-                device='cpu')
+                           model_engine=model_engine,
+                           optimizer=optimizer,
+                           micro_batch_size_per_gpu=config.train.micro_batch_size_per_gpu,
+                           clip_grad_norm=config.train.clip_grad_norm,
+                           use_cache=config.train.use_cache,
+                           device='cpu')
 
     else:
         raise ValueError(f"Unknown algorithm: {config.train.alg_name}")
@@ -265,7 +300,7 @@ if __name__ == "__main__":
                         desc=f"Epoch {epoch + 1}/{config.train.num_epochs}"):
             total_eval_step += 1
             metric = alg.eval_step(data)
-            if total_eval_step % config.eval.log_interval == 0:
+            if total_eval_step % config.val.log_interval == 0:
                 print(f"Global step: {global_step}, Loss: {metric['loss']}")
 
         ########
