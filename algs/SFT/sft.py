afe04795990ccc270f6e315dbeb48305d836b4af
rasool <r.fakoor@gmail.com>
2025-12-15 13:45:45 -0800

    update loss_mask which is already update in the dataloader

--- algs/SFT/sft.py
+++ algs/SFT/sft.py
@@ -16,9 +16,9 @@ class SFT:
         # use cross entropy loss
         self.loss_fn = torch.nn.CrossEntropyLoss(reduction="none")
 
-    def compute_loss(self, logits, y, mask):
+    def compute_loss(self, logits, y, loss_mask):
         '''
-         This functions implements \sum_{i=1}^{N} log p(y_i|x_i)
+         This function implements \sum_{i=1}^{N} log p(y_i|x_i)
          y is target label [B, T -1]
          logits is model prediction [B, T -1, vocab_size]
         '''
@@ -36,12 +36,12 @@ class SFT:
 
         # We need to apply mask to loss to remove any things 
         # which should not be considered in loss (e.g., padding tokens)
-        mask = mask.view(-1).to(dtype=per_token_loss.dtype)  # [B * (T - 1)]
-        masked_loss = per_token_loss * mask
+        loss_mask = loss_mask.view(-1).to(dtype=per_token_loss.dtype)  # [B * (T - 1)]
+        masked_per_token_loss = per_token_loss * loss_mask
 
-        # To avoid gardient accumulation error caused by loss.mean(),
+        # To avoid gradient accumulation error caused by loss.mean(),
         # we use sum of loss instead but play with learning rate to account for this.
-        loss = masked_loss.sum()
+        loss = masked_per_token_loss.sum()
         return loss
 
     def forward(self, batch):
@@ -52,9 +52,11 @@ class SFT:
             The size of return variables logits, y, and mask would be [B, T -1]
         '''
         # batch is a dictionary, so we want to extract things we need from it
-        # input_ids/att_mask/pos_ids are [B, T]
+        # input_ids and att_mask are [B, T]
         input_ids = batch['seq_ids'].to(self.device)
         att_mask  = batch['seq_attn_mask'].to(self.device)
+        # loss_mask is [B, T -1]
+        loss_mask = batch['loss_mask'].contiguous().to(self.device)
 
         # if pos_ids is not provided, HF will add that automatically.
         pos_ids   = batch.get('position_ids', None)
@@ -70,41 +72,13 @@ class SFT:
         # [B, T, vocab_size]
         every_token_logits = output.logits
 
-        # Example
-        #   prompt = [a, b, c]
-        #   answer = [d, e, f, g, <eos>]
-        # The model input (length = T) is:
-        #   [a, b, c, d, e, f, g, <eos>]
-        #
-        # The y is the input shifted left by one (length = T-1):
-        #   [b, c, d, e, f, g, <eos>]
-        #
-        # The model output (length = T) is if outputs correct results:
-        #   [b, c, d, e, f, g, <eos>, X]
-        # We discard the final token (X) since it is beyond <eos>, leaving logits of shape [T-1, vocab_size].
-        #
-        # The mask is length T, e.g.:
-        #   [0, 0, 1, 1, 1, 1, 1, 0]
-        #
-        # The leading zeros correspond to prompt tokens -> no loss on them.
-        # The final zero corresponds to the <eos> token in our example.
-        #
-        # Since we drop the last output logit, we also drop the first mask value (which is zero anyway),
-        # producing a mask of shape [T-1] aligned with the logits and targets.
-
-
         # label would be input_ids shifted by one (input_ids[:, 1:])
         # so the size is [B, T-1]
         y = input_ids[:, 1:].contiguous()
-        # seq_ids = [prompt_ids + answer_ids + EOS]
-        # as we don't care about what is outputs of model for EOS token, we remove it from logits
+        # it is next token prediction, so we remove last token from logits
         logits = every_token_logits[:, :-1, :].contiguous()
 
-        # we need to make mask of size [B, T-1].\
-        # we drop the first mask value (which is zero anyway).
-        mask = batch['loss_mask'][:, 1:].contiguous().to(self.device)
-
-        return logits, y, mask
+        return logits, y, loss_mask
 
     def eval_step(self, micro_batch):
         '''
@@ -114,10 +88,10 @@ class SFT:
         self.model_engine.eval()
         with torch.no_grad():
             # forward pass per gpu/rank
-            logits, y, mask = self.forward(micro_batch)
+            logits, y, loss_mask = self.forward(micro_batch)
 
             # compute loss pass
-            loss = self.compute_loss(logits=logits, y=y, mask=mask)
+            loss = self.compute_loss(logits=logits, y=y, loss_mask=loss_mask)
             val_loss = loss.item()
 
         return {"loss": float(val_loss)}
@@ -132,10 +106,10 @@ class SFT:
         self.model_engine.train()
 
         # 1. forward pass per gpu/rank
-        logits, y, mask = self.forward(micro_batch)
+        logits, y, loss_mask = self.forward(micro_batch)
 
         # 2. compute loss pass
-        loss = self.compute_loss(logits=logits, y=y, mask=mask)
+        loss = self.compute_loss(logits=logits, y=y, loss_mask=loss_mask)
 
         # 3. backward step
         # DeepSpeed backward handles gradient accumulation logic automatically.
