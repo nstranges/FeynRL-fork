9ed7c8cd98c523913ca2f3ef9ebb2e5e1ccce5de
rasool <r.fakoor@gmail.com>
2025-12-29 16:00:05 -0800

    fix the bugs and make sure it returns required information for replay bugger

--- algs/PPO/vllm_rollout_engine.py
+++ algs/PPO/vllm_rollout_engine.py
@@ -3,12 +3,12 @@ import gc
 import ray
 from vllm import LLM, SamplingParams
 import numpy as np
-from typing import Optional, List, Callable
+from typing import Optional, List, Callable, Any, Dict
 
-@ray.remote(num_gpus=1, resources={"vllm": 1})
+@ray.remote(resources={"vllm": 1})
 class VLLMRolloutEngine:
     def __init__(self,
-                 model_name: str,
+                 model_path: str,
                  trust_remote_code: bool,
                  temperature: float,
                  max_tokens: int,
@@ -22,29 +22,75 @@ class VLLMRolloutEngine:
                  prompt_logprobs: Optional[int],
                  force_strict_on_policy: bool,
                  reward_func: Callable,
+                 tensor_parallel_size: int,
+                 eos_id: int,
                  ):
 
-        self.model_name = model_name
-        self.trust_remote_code = trust_remote_code
         # reward function
         self.reward_func = reward_func
+        self.tensor_parallel_size = int(tensor_parallel_size)
+        self.eos_id = eos_id
 
         # sampling config
-        self.temperature = temperature
-        self.max_tokens = max_tokens
-        self.n_samples = n_samples
-        self.top_p = top_p
-        self.top_k = top_k
+        self.temperature = float(temperature)
+        self.max_tokens = int(max_tokens)
+        self.n_samples = int(n_samples)
+        self.top_p = float(top_p)
+        self.top_k = int(top_k)
         self.seed = seed
-        self.ignore_eos = ignore_eos
+        self.ignore_eos = bool(ignore_eos)
         self.stop = stop
         self.stop_token_ids = stop_token_ids
         self.prompt_logprobs = prompt_logprobs
-        self.force_strict_on_policy = force_strict_on_policy
+        self.force_strict_on_policy = bool(force_strict_on_policy)
 
-        self.vllm_engine = LLM(model=self.model_name, trust_remote_code=self.trust_remote_code)
+        # vllm engine config
+        self.model_path = model_path
+        self.loaded_version = -1
+        self.trust_remote_code = trust_remote_code
+        self.vllm_engine = None
+        self.refresh_model(model_path, 0)
         self.sampling_params = self.make_sampling_params()
 
+
+    def refresh_model(self, model_path: str, version: int) -> bool:
+        '''
+           refresh only if the model is changed.
+        '''
+        if self.vllm_engine is not None and \
+           self.loaded_version == version and \
+           model_path == self.model_path:
+            return False
+
+        self.model_path     = model_path
+        self.loaded_version = version
+        self.load_model()
+        return True
+
+    def load_model(self) -> None:
+        '''
+           load the model.
+        '''
+        if self.vllm_engine is not None:
+            # delete the old engine and free up memory
+            try:
+                del self.vllm_engine
+            except Exception as e:
+                print(f"Error deleting vllm_engine: {e}")
+                pass
+
+            self.vllm_engine = None
+            gc.collect()
+            try:
+                torch.cuda.empty_cache()
+            except Exception:
+                pass
+
+        self.vllm_engine = LLM(
+                                model=self.model_path,
+                                trust_remote_code=self.trust_remote_code,
+                                tensor_parallel_size=self.tensor_parallel_size
+                              )
     
     def make_sampling_params(self) -> SamplingParams:
         '''
@@ -98,16 +144,49 @@ class VLLMRolloutEngine:
             logits_processors=None,
 
             # setup to returns required info
-            logprobs=1, # it will return logprobs for each token 
-            prompt_logprobs=self.prompt_logprobs,
+            logprobs=1, # it returns logprobs for each token
+            prompt_logprobs=self.prompt_logprobs, # it returns logprobs for each token in the prompt which is memory intensive
         )
 
-    def collect(self, 
-                prompt_ids: List[Dict[str, List[int]]], 
-                current_iter: int, 
+    def extract_logprobs(self, response_ids: List[int], logprobs_by_pos: Any) -> torch.Tensor:
+        '''
+           Extract logprobs for each token in response_ids from logprobs.
+           logprobs_by_pos: list of dict {token_id -> logprob_info}
+        '''
+        if logprobs_by_pos is None or len(response_ids) != len(logprobs_by_pos):
+            raise ValueError("logprobs_by_pos must be a list of dict with the same len as response_ids.")
+
+        token_logprobs = []
+        for t_id, lgp_dict in zip(response_ids, logprobs_by_pos):
+            if lgp_dict is None:
+                raise ValueError(f"No logprobs for token {t_id} in {response_ids}.")
+
+            key = t_id
+            if key not in lgp_dict and str(key) in lgp_dict:
+                key = str(key)
+
+            if key not in lgp_dict:
+                raise ValueError(f"No logprobs for token {t_id} in {response_ids}.")
+
+            # account for different formats of logprobs
+            v = lgp_dict[key]
+            if hasattr(v, 'logprob'):
+                token_logprobs.append(float(v.logprob))
+
+            elif isinstance(v, dict) and 'logprob' in v:
+                token_logprobs.append(float(v['logprob']))
+
+            else:
+                raise TypeError(f"Unexpected logprob type: {type(v)}")
+
+        return torch.tensor(token_logprobs, dtype=torch.float32, device='cpu')
+
+    def generate(self,
+                prompts: List[Dict[str, List[int]]],
+                current_iter: int,
                 policy_version: int) -> List[Dict[str, Any]]:
                 ''' 
-                    prompt_ids: [{'prompt_token_ids': [2,..]}, {'prompt_token_ids': [...]}, ...]
+                    prompts: [{'prompt_token_ids': [2,..]}, {'prompt_token_ids': [...]}, ...]
                     Returns a list of rollout samples. length ~ B * n_samples.
                     Each sample includes:
                       - input_ids (prompt+response)
@@ -115,63 +194,85 @@ class VLLMRolloutEngine:
                       - old_logprobs (per response token)
                       - finish_reason
                 '''
-
-                generated_outputs = self.vllm_engine.generate(prompt_ids, 
-                                                             self.sampling_params)
+                assert self.vllm_engine is not None, f"{self.model_path} not loaded."
+                generated_outputs = self.vllm_engine.generate(prompts,
+                                                             sampling_params=self.sampling_params,
+                                                             use_tqdm=False)
                 # generated_outputs has prompt_ids and other outputs
-                # this works of even if n_samples >= 1
+                # this works even if n_samples >= 1
                 rollout_samples = []
                 for data in generated_outputs:
-                    prompt_ids = data.prompt_token_ids if data.prompt_token_ids is not None else []
+                    prompt_ids = list(data.prompt_token_ids or [])
                     prompt_len = len(prompt_ids)
-                    
+                    if prompt_len == 0:
+                        raise ValueError(f"No prompt token ids found in generated output: {data}")
+
                     # process generated responses
                     for response in data.outputs:
                         response_ids = list(response.token_ids)
                         response_len = len(response_ids)
+                        finish_reason = getattr(response, "finish_reason", None)
+                        stop_reason   = getattr(response, "stop_reason", None)
 
-                        # 1. now get data for policy update like old_log_probs, rewards, etc.
-                        logprobs = response.logprobs
-                        
-                        # 2. build input_ids (prompt + response) on cpu
+                        # all of the following are on cpu and have length (prompt_len + response_len)
+                        # build input_ids (prompt + response) on cpu
+                        seq_len = prompt_len + response_len
                         input_ids = torch.tensor(prompt_ids + response_ids, dtype=torch.int32, device='cpu')
-                        mask      = torch.zeros((prompt_len + response_len,), dtype=torch.int32, device='cpu')
+                        mask      = torch.zeros((seq_len,), dtype=torch.int32, device='cpu')
+                        done      = torch.zeros((seq_len,), dtype=torch.int32, device='cpu')
+                        old_logps = torch.zeros((seq_len,), dtype=torch.float32, device='cpu')
+                        rewards   = torch.zeros((seq_len,), dtype=torch.float32, device='cpu')
+
                         if response_len > 0:
+                            # 1 if valid token which we want to update.
                             mask[prompt_len:] = 1
 
-                        # 1 if t is EOS (terminal), 0 otherwise.
-                        done  = torch.zeros((prompt_len + response_len,), dtype=torch.int32, device='cpu')
-                        # possible values for finish_reason in vLLM:
-                        #   stop: generation stopped because a stop condition was met like EOS
-                        #   length: generation stopped because max_tokens was reached
-                        #   abort: generation stopped because of an error
-                        #   None: for unknown reason
-                        if "finish_reason" in response and response.finish_reason == "stop": 
-                            done[-1] = 1
-                        
-                        # get rewards per token same as length as input_ids
-                        rewards = self.calculate_rewards(prompt_ids, response_ids, response.finish_reason)
-                        
-                        # 3. build old_logprobs
-                        old_logprobs = logprobs[prompt_len:]
-                        
-                        # 5. build rollout sample
+                            if response.logprobs is None:
+                                raise ValueError("response.logprobs is None. Check if SamplingParams(logprobs=1) is set.")
+
+                            # now get data for policy update like old_log_probs, rewards, etc.
+                            response_logprobs = self.extract_logprobs(response_ids, response.logprobs)
+                            old_logps[prompt_len:] = response_logprobs
+                            rewards   = self.score_response(prompt_ids, response_ids, finish_reason)
+
+                            # Terminal handling:
+                            #  1. stop: ended due to EOS or a stop condition
+                            #  2. length: truncated which should not be done and we need to bootstrap
+                            if finish_reason == "stop":
+                                done[seq_len -1] = 1
+
+                            eos_in_tokens = response_ids[-1] == self.eos_id
+                            ended_on_eos = (finish_reason == "stop" and stop_reason is None and eos_in_tokens)
+
+                        else:
+                            ended_on_eos = False
+
+                        # rollout sample
                         rollout_samples.append({
-                            "iter": int(current_iter),
-                            "policy_version": int(policy_version),
-                            "input_ids": input_ids,
-                            "mask": mask,
-                            "done": done,
-                            "old_logprobs": old_logprobs,
-                            "response_text": response.text,  
-                        })
+                                                "iter": int(current_iter),
+                                                "policy_version": int(policy_version),
+                                                "loaded_version": int(self.loaded_version),
+
+                                                "input_ids": input_ids, #[T]
+                                                "mask": mask, #[T] 1 on response/valid tokens
+                                                "done": done, #[T] 1 on last token if terminal
+                                                "rewards": rewards, #[T]
+                                                "old_logps": old_logps, #[T] 0 on prompt since we don't backprop on it
+
+                                                "finish_reason": finish_reason,
+                                                "stop_reason": stop_reason,
+                                                "ended_on_eos": ended_on_eos,
+
+                                                "response_ids": response_ids,
+                                                "prompt_ids": prompt_ids,
+                                                "response_text": getattr(response, "text", ""),
+                                                })
 
                 return rollout_samples
 
-    def calculate_rewards(prompt_ids, response_ids, finish_reason) -> List[float]:
+    def score_response(self, prompt_ids, response_ids, finish_reason) -> List[float]:
         '''
             Calculate the reward for each response token.
-            reward function assumes getting the prompt_ids, response_ids and finish_reason
-            as input.
+            it returns a float tensor of [T] where assign zero to prompt tokens.
         '''
         return self.reward_func(prompt_ids, response_ids, finish_reason)
