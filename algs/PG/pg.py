6d97b6c5cf86585344283367eab08b87737d9330
rasool <r.fakoor@gmail.com>
2026-01-05 10:45:57 -0800

    init commity

---
+++ algs/PG/pg.py
@@ -0,0 +1,193 @@
+import torch
+import numpy as np
+from tqdm import tqdm
+import ray
+
+@ray.remote(resources={"training": 1})
+class GRPO:
+    def __init__(self,
+                policy_engine,
+                kl_coeff: float,
+                clip_low: float,
+                clip_high: float,
+                entropy_coeff: float,
+                use_cache: bool,
+                micro_batch_size_per_gpu: int,
+                ref_model=None,
+                ):
+
+        # model related parameters
+        self.policy_engine = policy_engine
+        self.ref_model = ref_model
+        self.use_cache = use_cache
+        self.micro_batch_size_per_gpu = micro_batch_size_per_gpu
+
+        # policy related parameters
+        self.kl_coeff = float(kl_coeff)
+        self.clip_low = float(clip_low)
+        self.clip_high = float(clip_high)
+        self.ent_coeff = float(entropy_coeff)
+
+        # use cross entropy loss for policy gradient
+        self.cross_entropy = torch.nn.CrossEntropyLoss(reduction="none")
+
+
+    def policy_forward(self, input_ids, att_mask, pos_ids):
+        '''
+            input_ids and att_mask are [B, T]
+            pos_ids is [B, T] or None
+            Returns:
+                logits is [B, T-1, vocab_size]
+                entropies is [B, T-1]
+        '''
+        # if pos_ids is not provided, HF will add that automatically.
+        if pos_ids is not None:
+            pos_ids = pos_ids.to(input_ids.device)
+
+        # feed data to model
+        output = self.policy_engine(input_ids=input_ids,
+                                   attention_mask=att_mask,
+                                   position_ids=pos_ids,
+                                   use_cache=self.use_cache)
+
+        # [B, T, V] -> [B, T-1, V]
+        logits = output.logits[:, :-1, :].contiguous()
+        B, T_minus_1, vocab_size = logits.shape
+
+        # [B, T] -> [B, T-1]
+        target_ids = input_ids[:, 1:].contiguous()
+
+        # cross_entropy return -logprobs but we need logprobs
+        # logits is [B, T-1, vocab_size]
+        # target_ids is [B, T-1]
+        neg_logprobs = self.cross_entropy(logits.view(-1, vocab_size), target_ids.view(-1))
+        logprobs = -neg_logprobs.view(B, T_minus_1)
+        # we can also do this, but it is less efficient I guess
+        #   logprobs = logits.log_softmax(dim=-1)
+        #   logprobs = torch.gather(logprobs, dim=-1, index=target_ids)
+
+        entropies = None
+        if self.ent_coeff > 0.0:
+            entropies = torch.distributions.Categorical(logits=logits).entropy()
+
+        return logprobs, entropies
+
+    def compute_policy_loss(self,
+                            logprobs: torch.Tensor,
+                            old_logprobs: torch.Tensor,
+                            advantages: torch.Tensor,
+                            mask: torch.Tensor,
+                            entropies: torch.Tensor,
+                            ):
+        '''
+            logprobs, old_logprobs, advantages, mask: [B, T]
+            Compute policy loss:
+                1. ratio = exp(logprobs - old_logprobs)
+                2. loss = -(min(ratio * adv, clip_adv * adv)) * mask
+        '''
+        device = logprobs.device
+        dtype = logprobs.dtype
+        loss_ent = torch.tensor(0.0, device=device, dtype=dtype)
+
+        # 1. make sure advantages are detached and
+        # convert to float32 for stability under bf16/fp16
+        adv = advantages.detach().to(torch.float32)
+        mask = (mask.to(device=device) > 0.5).to(dtype=dtype)
+        denom = mask.sum().clamp(min=1.0)
+
+        # 2. calculate ratio = exp(logprobs - old_logprobs)
+        logratio = (logprobs - old_logprobs).to(torch.float32)
+        ratio   = torch.exp(logratio)
+
+        # 3. compute loss: -(min(ratio * adv, clip_adv)) * mask
+        unclipped = ratio * adv
+        clip_adv  = torch.clamp(ratio, 1.0 - self.clip_low, 1.0 + self.clip_high) * adv
+        loss_pi   = -(torch.minimum(unclipped, clip_adv) * mask).sum() / denom
+
+        # 4. compute entropy loss
+        if entropies is not None and self.ent_coeff > 0.0:
+            loss_ent = (entropies * mask).sum() / denom
+
+        loss_total = loss_pi - self.ent_coeff * loss_ent
+
+        # 5. useful metrics
+        with torch.no_grad():
+            # first term too large ==> policy changed too much upward
+            # second term too small ==> policy changed too much downward
+            clipped_mask = (ratio > (1.0 + self.clip_high)) | (ratio < (1.0 - self.clip_low))
+            # fraction of masked tokens that ratio out of ranges
+            clipfrac = (clipped_mask.to(dtype=dtype) * mask).sum() / denom
+
+            # approx KL: either E[old_logprobs - logprobs] or E[(ratio - 1) - logratio]
+            approx_kl_t = (ratio - 1.0) - logratio
+            approx_kl = (approx_kl_t.to(dtype=dtype) * mask).sum() / denom
+
+            # save the metrics for debugging
+            metrics = {
+                'clipfrac': clipfrac,
+                'approx_kl': approx_kl,
+                'loss_ent': loss_ent.item(),
+                'loss_pi': loss_pi.item(),
+                'loss_total': loss_total.item(),
+            }
+
+        return loss_total, metrics
+
+    def train_step(self, replay_buffer):
+        '''
+           This function implements a training step per rank/gpu for full replay buffer.
+           The batch size for each gpu/rank should be micro_batch_size_per_gpu.
+        '''
+        device = self.policy_engine.device
+
+        # 1. Models to train mode
+        self.policy_engine.train()
+
+        # 2. zero grads
+        self.policy_engine.zero_grad()
+
+        # 3. create progress bar
+        num_micro = len(replay_buffer) # replay_buffer is already a dataloader of micro-batches
+        progress_bar = tqdm(replay_buffer, total=num_micro)
+
+        ga_pi_attr = getattr(self.policy_engine, 'gradient_accumulation_steps', 1)
+        ga_pi = int(ga_pi_attr() if callable(ga_pi_attr) else ga_pi_attr)
+
+        for step, micro_batch in enumerate(progress_bar):
+            is_last = (step == (num_micro - 1))
+            is_boundary = (((step + 1) % ga_pi) == 0) or is_last
+
+            ########
+            # 1. Data from buffer
+            ########
+            rewards   = micro_batch['rewards'].to(device, non_blocking=True)
+            done      = micro_batch['done'].to(device, non_blocking=True)
+            mask      = micro_batch['mask'].to(device, non_blocking=True)
+            old_logprobs = micro_batch['old_logprobs'].to(device, non_blocking=True)
+
+            input_ids = micro_batch['input_ids'].to(device, non_blocking=True)
+            att_mask  = micro_batch['attn_mask'].to(device, non_blocking=True)
+            pos_ids   = micro_batch.get('position_ids', None)
+
+            ########
+            # 2. Compute advantages
+            ########
+            # Forward pass through the current policy.
+            pi_logprobs, pi_entropies = self.policy_forward(
+                                                                input_ids=input_ids,
+                                                                att_mask=att_mask,
+                                                                pos_ids=pos_ids)
+
+            # Compute policy loss using the current policy.
+            pi_loss, pi_metrics = self.compute_policy_loss(
+                                                        logprobs=pi_logprobs,
+                                                        old_logprobs=old_logprobs,
+                                                        advantages=advantages,
+                                                        mask=mask,
+                                                        entropies=pi_entropies)
+
+            self.policy_engine.set_gradient_accumulation_boundary(is_boundary)
+
+            # backward pass
+            self.policy_engine.backward(pi_loss)
+            self.policy_engine.step()
