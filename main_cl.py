a5c4130a4beb98ca5171931dc7bf3fd9e5fcd888
rasool <r.fakoor@gmail.com>
2026-03-25 04:46:34 -0700

    add back gc.collect which was remvoed accidentally

--- main_cl.py
+++ main_cl.py
@@ -572,6 +572,8 @@ if __name__ == "__main__":
     del model_engine
     del ref_model_engine
     del optimizer
+    gc.collect()
+
     if torch.cuda.is_available():
         torch.cuda.synchronize()
         torch.cuda.empty_cache()
