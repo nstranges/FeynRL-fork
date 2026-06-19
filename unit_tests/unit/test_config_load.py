b542843052071589637a16a6304e3892574535cd
Murdock Aubry <murdockaubry@gmail.com>
2026-06-19 18:15:28 +0000

    lint line

--- unit_tests/unit/test_config_load.py
+++ unit_tests/unit/test_config_load.py
@@ -177,4 +177,5 @@ def test_ppo_direct_sync_requires_checkpoint_save_interval_1(tmp_path):
     with open(config_file, "w") as f:
         yaml.dump(config_dict, f)
     config = load_and_verify(method="rl", input_yaml=str(config_file), experiment_id="e", rank=0)
+    assert config.run.checkpoint_save_interval == 5
     
